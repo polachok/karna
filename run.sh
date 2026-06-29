a29349d6afcd41f4cfc38bd1732c2e0dcea0806d
Alexander Polakov <alex@plhk.dev>
2026-06-29 22:18:20 +0200

    enable smp in qemu for funsies

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 aarch64-none-elf-objcopy -O binary "$1" "$1.bin"
 exec qemu-system-aarch64 -M virt,gic-version=2 -cpu cortex-a72 -m 512M \
-    -nographic -kernel "$1.bin"
+   -smp 2 -nographic -kernel "$1.bin"
